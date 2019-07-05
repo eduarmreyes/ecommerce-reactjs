f8f6d8ea4a6e674ec70c6226f0f0d29bc3d65b55
eduarmreyes <eduar.mreyes@gmail.com>
2019-07-05 07:19:12 -0400

    :tada: add product list component

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,5 +1,6 @@
 import React from "react";
-import data from "./data.json";
+import axios from "axios";
+import ROOT_API from "../../constants/constants";
 import "./ProductList.css";
 
 class ProductList extends React.Component {
@@ -8,7 +9,22 @@ class ProductList extends React.Component {
     this.state = { data: [] };
   }
   componentDidMount() {
-    this.setState({ data: data });
+    axios
+      .get(ROOT_API + "products", {
+        params: {
+          page: 1,
+          limit: 20,
+          description_length: 200
+        }
+      })
+      .then(response => {
+        this.setState({
+          data: response.data
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      });
   }
   render() {
     const product_list = this.state.data.rows
@@ -17,9 +33,9 @@ class ProductList extends React.Component {
         })
       : "";
     return (
-      <div>
+      <section className="product_list">
         <ul>{product_list}</ul>
-      </div>
+      </section>
     );
   }
 }
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+const ROOT_API = "https://backendapi.turing.com/";
+
+export default ROOT_API;
